d82cb724134e7709cd79cf07b9f950c5060627f8
Jean-Martin Archer <git@jmartin.ca>
2013-11-18 20:50:46 -0800

    uglifying everything

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+/* jshint node: true */
+
+module.exports = function(grunt) {
+    'use strict';
+    // Project configuration.
+    grunt.initConfig({
+        uglify: {
+            target: {
+                files: {
+                    './js/min.js': [
+                        './lib/jquery-2.0.3.js',
+                        './lib/bootstrap-3.0.2/js/bootstrap.js',
+                        './lib/angular-1.2.0/angular.js',
+                        './lib/angular-1.2.0/angular-animate.js',
+                        './lib/angular-1.2.0/angular-route.js',
+                    ],
+                }
+            }
+        },
+        cssmin: {
+            combine: {
+                files: {
+                    './css/min.css': [
+                    	'./lib/bootstrap-3.0.2/css/bootstrap.css',
+                        './lib/font-awesome-4.0.3/css/font-awesome-stripped.css',
+                        './css/more.css'
+                    ]
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.registerTask('default', ['uglify', 'cssmin']);
+
+};
