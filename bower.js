2cfd1ceabdf593c111befaa816a61d4a27529b4b
Jean-Martin <git@jmartin.ca>
2013-11-21 11:34:59 -0800

    bower

---
+++ bower.js
@@ -0,0 +1,16 @@
+{
+  "name": "jmartin.ca",
+  "version": "0.0.1",
+  "main": [
+    "./*"
+  ],
+  "ignore": [
+    "**/.*"
+  ],
+  "dependencies": {
+    "angular-latest": ">= 1.2.0",
+    "jquery": ">= 2.0.3",
+    "bootstrap": "3.0.2",
+    "jquery.lazyload": "1.9.1"
+  }
+}
