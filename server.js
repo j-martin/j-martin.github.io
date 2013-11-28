3606658b49a4cd3555ba78559fdc85e8caf937f5
Jean-Martin <git@jmartin.ca>
2013-11-28 12:01:43 -0800

    major cleanup

--- server.js
+++ server.js
@@ -1,25 +1,34 @@
 #!/bin/env node
+
+'strict';
+/*global express:false */
+/*global process:false */
+
 //Get the environment variables we need.
-var ipaddr  = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1";
-var port    = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 5000;
+var ipaddr = process.env.OPENSHIFT_NODEJS_IP;
+var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 5000;
 
-var express = require("express"),
-    app     = express()
-    
-app.get("/", function(req, res) {
-  res.redirect("index.html");
+var express = require('express'),
+    app = express()
+
+    app.get('/', function(req, res) {
+        res.redirect('index.html');
+    });
+
+app.configure(function() {
+    app.use(express.methodOverride());
+    app.use(express.bodyParser());
+    app.use(express.static(__dirname + '/'));
+    app.use(express.errorHandler({
+        dumpExceptions: true,
+        showStack: true
+    }));
+    app.use(app.router);
 });
 
-app.configure(function(){
-  app.use(express.methodOverride());
-  app.use(express.bodyParser());
-  app.use(express.static(__dirname + '/'));
-  app.use(express.errorHandler({
-    dumpExceptions: true, 
-    showStack: true
-  }));
-  app.use(app.router);
-});
-
-app.listen(port, ipaddr);
-console.log("Server running at http://" + ipaddr + ":" + port + "/");
+if (!ipaddr) {
+    app.listen(port);
+} else {
+    app.listen(port, ipaddr);
+}
+console.log('Server running on port: ' + port + ' !');
