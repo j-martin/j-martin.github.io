f09584aaab530338ad8cf6de7b1534e0a63c1ed5
Jean-Martin <git@jmartin.ca>
2013-11-28 12:07:13 -0800

    fixing heroku

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
-var express = require("express");
-var app = express();
-app.use(express.logger());
+#!/bin/env node
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+'strict';
+/*global express:false */
+/*global process:false */
+
+//Get the environment variables we need.
+var ipaddr = process.env.OPENSHIFT_NODEJS_IP;
+var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 5000;
+
+var express = require('express'),
+    app = express()
+
+    app.get('/', function(req, res) {
+        res.redirect('index.html');
+    });
+
+app.configure(function() {
+    app.use(express.methodOverride());
+    app.use(express.bodyParser());
+    app.use(express.static(__dirname + '/'));
+    app.use(express.errorHandler({
+        dumpExceptions: true,
+        showStack: true
+    }));
+    app.use(app.router);
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+if (!ipaddr) {
+    app.listen(port);
+} else {
+    app.listen(port, ipaddr);
+}
+console.log('Server running on port: ' + port + ' !');
