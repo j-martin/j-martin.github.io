ee15ea8f3c531421231b3492a786688a4c18d65c
Jean-Martin <git@jmartin.ca>
2013-12-04 14:07:58 -0800

    cleanup

--- fabfile.py
+++ fabfile.py
@@ -2,49 +2,53 @@
 
 """fabfile.py: Servers Maintenance"""
 
-__author__      = "Jean-Martin Archer"
-__copyright__   = "MIT License"
+__author__ = "Jean-Martin Archer"
+__copyright__ = "MIT License"
 
 
 from fabric.api import run, local, env, cd
 
+
 def commit_push():
-	"""
-	Local commit and push to GitHub before updating everything else.
-	"""
+    """
+    Local commit and push to GitHub before updating everything else.
+    """
 
-	local('git commit -am pushing\ to\ other\ servers.')
-	local('git push')
-	return True
+    local('git commit -am pushing\ to\ other\ servers.')
+    local('git push')
+    return True
 
-def staging_pull(hosts = ['alpha']):
-	"""
-	Connects to the home server which updates the other servers.
 
-	Once the repo has been pulled the Puppet agents running on other servers
-	will also update themselves.
-	"""
-	
-	env.hosts = hosts
+def staging_pull(hosts=['alpha']):
+    """
+    Connects to the home server which updates the other servers.
 
-	with cd('/home/shared/website/'):
-		run ('git pull')
-		run ('git-ftp -s main push')
-		run ('git-ftp -s europe push')
-	return True
+    Once the repo has been pulled the Puppet agents running on other servers
+    will also update themselves.
+    """
 
-def push2cloud(servers = ['heroku', 'rhc']):
-	for server in servers:
-		local('git push %s master' % server)
+    env.hosts = hosts
 
-	return True
+    with cd('/home/shared/website/'):
+        run('git pull')
+        run('git-ftp -s main push')
+        run('git-ftp -s europe push')
+    return True
+
+
+def push2cloud(servers=['heroku', 'rhc']):
+    for server in servers:
+        local('git push %s master' % server)
+
+    return True
+
 
 def big_push():
-	"""
-	Pushes everything to all the servers
-	"""
+    """
+    Pushes everything to all the servers
+    """
 
-	push2cloud()
-	staging_pull()
+    staging_pull()
+    push2cloud()
 
-	return True
+    return True
